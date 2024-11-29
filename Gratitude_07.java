677d09dfc9a9571075a064c919b4f349618f0b01
07nawraa <anindyanauraanindya@gmail.com>
2024-11-29 11:58:44 +0700

    Ini jawaban experiment 2

--- Gratitude_07.java
+++ Gratitude_07.java
@@ -5,7 +5,17 @@ public class Gratitude_07 {
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 
+    // public static void main(String[] args) {
+    //     sayThankyou();
+    // }
+
+    public static void sayAdditionalGreetings(String greeting) {
+        System.out.println(greeting);
+    }
+
     public static void main(String[] args) {
         sayThankyou();
+        String expression = "Thankyou... wish you all the best!!";
+        sayAdditionalGreetings(expression);
     }
 }
